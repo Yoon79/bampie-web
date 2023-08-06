3fbebbc978bacd2e79f3bae7a3cd25de6350f9ec
Minhyuk <minhyukit@gmail.com>
2023-08-06 15:27:38 +0900

    confetti.tsx

--- component/confetti.tsx
+++ component/confetti.tsx
@@ -2,23 +2,21 @@
 import { useCallback, useEffect, useRef } from 'react';
 import ReactCanvasConfetti from 'react-canvas-confetti';
 
-export default function Confetti(this: any) {
-  const refAnimationInstance = useRef(null);
+export default function Confetti() {
+  const refAnimationInstance = useRef<any | null>(null);
 
-  const getInstance = useCallback((instance: null)  => {
+  const getInstance = useCallback((instance: any | null) => {
     refAnimationInstance.current = instance;
   }, []);
 
   const makeShot = useCallback((particleRatio: number, opts: any) => {
     refAnimationInstance.current &&
-      refAnimationInstance.current({
-        ...opts,
-        origin: { y: 0.7 },
-        particleCount: Math.floor(200 * particleRatio)
-      });
+    refAnimationInstance.current({
+      ...opts,
+      origin: { y: 0.7 },
+      particleCount: Math.floor(200 * particleRatio)
+    });
   }, []);
-
-  useEffect(() => fire(), []);
 
   const fire = useCallback(() => {
     makeShot(0.25, {
@@ -49,17 +47,21 @@ export default function Confetti(this: any) {
     });
   }, [makeShot]);
 
+  useEffect(() => {
+    fire();
+  }, [fire]);
+
   return (
-    <ReactCanvasConfetti
-      refConfetti={getInstance}
-      style={{
-        position: 'fixed',
-        pointerEvents: 'none',
-        width: '100%',
-        height: '100%',
-        top: 0,
-        left: 0
-      }}
-    />
+      <ReactCanvasConfetti
+          refConfetti={getInstance}
+          style={{
+            position: 'fixed',
+            pointerEvents: 'none',
+            width: '100%',
+            height: '100%',
+            top: 0,
+            left: 0
+          }}
+      />
   );
 }
